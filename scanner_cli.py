e94c9e2e801ebfe74c400cdec91502c8734f4c53
Artem <artem.skripnik2004@gmail.com>
2021-07-26 21:20:20 +0300

    change naming style to snake_case in scanner_cli.py

--- scanner_cli.py
+++ scanner_cli.py
@@ -5,22 +5,23 @@ import utils
 
 __version__ = '1.0.0'
 
-PARSER = ArgumentParser(
+parser = ArgumentParser(
     description="Script for scanning devices for open ports")
-
-PARSER.add_argument('-v', '--version', action='version',
-                    version=f"{PARSER.prog}  {__version__} ")
-PARSER.add_argument('ip', help="IP for scanning (example: 192.168.1.1)")
-PARSER.add_argument('-p', '--ports', default='1-10000',
+parser.add_argument('-v', '--version', action='version',
+                    version=f"{parser.prog}  {__version__} ")
+parser.add_argument('ip', help="IP for scanning (example: 192.168.1.1)")
+parser.add_argument('-p', '--ports', default='1-10000',
                     help="port range for scanning (example: 1-10000), default: 1-10000)")
-PARSER.add_argument('-f', '--file', help="Save results to file")
-ARGS = PARSER.parse_args()
+parser.add_argument('-f', '--file', help="Save results to file")
 
-IP = ARGS.ip
-PORTS = list(map(int, ARGS.ports.split('-')))
-FILE = ARGS.file
+args = parser.parse_args()
 
-SCANNER = utils.Scanner(IP)
-OPENED_PORTS = SCANNER.scan_ports_range(*PORTS, FILE)
-print(f"IP: {IP}, ports: {PORTS[0]}->{PORTS[1]}")
-print("Opened ports:", *OPENED_PORTS, sep="\n")
+# TODO add ports range and IP validation
+ip = args.ip
+ports = list(map(int, args.ports.split('-')))
+file = args.file
+
+scanner = utils.Scanner(ip)
+opened_ports = scanner.scan_ports_range(*ports, file)
+print(f"IP: {ip}, ports: {ports[0]}->{ports[1]}")
+print("Opened ports:", *opened_ports, sep="\n")
